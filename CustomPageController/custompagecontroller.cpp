3fd1ff9068e9d90eb5a65279dcccbc83a09873a6
liupeng2015bj <liupeng2015bj@163.com>
2021-01-19 16:17:30 +0800

    添加页面控制器

---
+++ CustomPageController/custompagecontroller.cpp
@@ -0,0 +1,151 @@
+﻿#include "custompagecontroller.h"
+#include "ui_custompagecontroller.h"
+#include <QListView>
+
+CustomPageController::CustomPageController(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::CustomPageController)
+{
+    ui->setupUi(this);
+    InitUI();
+    ConnectEvents();
+    SetStyleSheet();
+}
+
+CustomPageController::~CustomPageController()
+{
+    delete ui;
+}
+
+void CustomPageController::SetNumberOfPage(QStringList numberList)
+{
+    ui->comboBox->clear();
+    ui->comboBox->addItems(numberList);
+    ui->comboBox->setCurrentText(numberList.at(0));
+}
+
+void CustomPageController::SetTotalNumber(int totalNum)
+{
+    m_totalNum = totalNum;
+    int number = ui->comboBox->currentText().toInt();
+    m_totalPage = m_totalNum / number;
+	if (m_totalNum % number > 0)
+	{
+		m_totalPage += 1;
+	}
+
+	if (m_totalPage < 1)
+	{
+		m_totalPage = 1;
+	}
+
+	ui->lineEdit->setText("1");
+    ui->label_total->setText(QStringLiteral("/%1页").arg(m_totalPage));
+}
+
+int CustomPageController::GetCurrentNumber()
+{
+    return ui->comboBox->currentText().toInt();
+}
+
+void CustomPageController::InitUI()
+{
+    QStringList numberList;
+    numberList << "25" << "50" << "100";
+    ui->comboBox->addItems(numberList);
+    ui->lineEdit->setText("1");
+
+    m_totalNum = 0;
+    m_totalPage = 1;
+}
+
+void CustomPageController::ConnectEvents()
+{
+    connect(ui->comboBox,SIGNAL(currentIndexChanged(int)),this,SLOT(slotCurrentNumberChanged(int)));
+    connect(ui->Btn_home,SIGNAL(clicked(bool)),this,SLOT(slotBtnHomePageClicked()));
+    connect(ui->Btn_last,SIGNAL(clicked(bool)),this,SLOT(slotBtnLastPageClicked()));
+    connect(ui->Btn_previous,SIGNAL(clicked(bool)),this,SLOT(slotBtnPrevPageClicked()));
+    connect(ui->Btn_next,SIGNAL(clicked(bool)),this,SLOT(slotBtnNextPageClicked()));
+    connect(ui->lineEdit,SIGNAL(returnPressed()),this,SLOT(slotCurrentPageChanged()));
+}
+
+void CustomPageController::SetStyleSheet()
+{
+    ui->comboBox->setView(new QListView());
+    ui->comboBox->setStyleSheet("QComboBox QAbstractItemView::item { min-height:32px; }");
+}
+
+void CustomPageController::slotCurrentNumberChanged(int index)
+{
+    int number = ui->comboBox->currentText().toInt();
+    if(number == 0)
+    {
+        return;
+    }
+    m_totalPage = m_totalNum / number;
+
+	if (m_totalNum % number > 0)
+	{
+		m_totalPage += 1;
+	}
+
+	if (m_totalPage < 1)
+	{
+		m_totalPage = 1;
+	}
+    ui->label_total->setText(tr("/%1 page").arg(m_totalPage));
+    ui->lineEdit->setText("1");
+    emit sigPageChanged(1);
+}
+
+void CustomPageController::slotCurrentPageChanged()
+{
+    int currentPage = ui->lineEdit->text().toInt();
+    if (currentPage < 1)
+    {
+        currentPage = 1;
+    }
+
+    if(currentPage > m_totalPage)
+    {
+        currentPage = m_totalPage;
+    }
+
+    emit sigPageChanged(currentPage);
+}
+
+void CustomPageController::slotBtnHomePageClicked()
+{
+    ui->lineEdit->setText("1");
+    emit sigPageChanged(1);
+}
+
+void CustomPageController::slotBtnLastPageClicked()
+{
+    ui->lineEdit->setText(QString::number(m_totalPage));
+    emit sigPageChanged(m_totalPage);
+}
+
+void CustomPageController::slotBtnPrevPageClicked()
+{
+    int currentPage = ui->lineEdit->text().toInt() - 1;
+    if (currentPage < 1)
+    {
+        currentPage = 1;
+    }
+
+    ui->lineEdit->setText(QString::number(currentPage));
+    emit sigPageChanged(currentPage);
+}
+
+void CustomPageController::slotBtnNextPageClicked()
+{
+    int currentPage = ui->lineEdit->text().toInt() + 1;
+    if (currentPage > m_totalPage)
+    {
+        currentPage = m_totalPage;
+    }
+
+    ui->lineEdit->setText(QString::number(currentPage));
+    emit sigPageChanged(currentPage);
+}
---
+++ CustomPageController/custompagecontroller.h
@@ -0,0 +1,43 @@
+﻿#ifndef CUSTOMPAGECONTROLLER_H
+#define CUSTOMPAGECONTROLLER_H
+
+#include <QWidget>
+
+namespace Ui {
+class CustomPageController;
+}
+
+class CustomPageController : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit CustomPageController(QWidget *parent = 0);
+    ~CustomPageController();
+    void SetNumberOfPage(QStringList numberList);
+    void SetTotalNumber(int totalNum);
+    int GetCurrentNumber();
+
+private:
+    void InitUI();
+    void ConnectEvents();
+    void SetStyleSheet();
+
+private slots:
+    void slotCurrentNumberChanged(int index);
+    void slotCurrentPageChanged();
+    void slotBtnHomePageClicked();
+    void slotBtnLastPageClicked();
+    void slotBtnPrevPageClicked();
+    void slotBtnNextPageClicked();
+
+signals:
+    void sigPageChanged(int currentPage);
+
+private:
+    Ui::CustomPageController *ui;
+    int m_totalNum;
+    int m_totalPage;
+};
+
+#endif // CUSTOMPAGECONTROLLER_H
